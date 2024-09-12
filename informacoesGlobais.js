a4c196c405a55003ae8e474c3134410479f1dd88
izaZocche <111132720+izaZocche@users.noreply.github.com>
2024-09-12 11:36:54 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,12 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais () {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e 
+    que aproximadamente`
+}
+
+visualizarInformacoesGlobais ()
